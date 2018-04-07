0770fc30e161195c07332ccf2afaba48a02ad25d
jaeregzz <jtregan@uvm.edu>
2018-04-07 11:10:45 -0400

    Update home.php

--- src/app/home.php
+++ src/app/home.php
@@ -12,7 +12,61 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-        <div id='map' style='width: 960px; height: 600px ; margin-left: .05%'></div>
+ 
+  <ol>
+    <li><a href="lost.php">lost.php</a></li>
+    <li><a href="found.php">found.php</a></li>
+    <li><a href="about.php">about.php</a></li>
+  </ol>
+
+  <div class="container">
+    <!-- Trigger the modal with a button -->
+    <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Lost</button>
+    <button type="button2" class="btn btn-info btn-lg" data-toggle="modal"
+    data-target="#myModal2">Found</button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="myModal" role="dialog">
+      <div class="modal-dialog">
+      
+        <!-- Modal content-->
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <h4 class="modal-title">Lost</h4>
+          </div>
+          <div class="modal-body">
+            <p>Enter Lost Item.</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+    <div class="modal fade" id="myModal2" role="dialog">
+      <div class="modal-dialog">
+      
+        <!-- Modal content-->
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button2" class="close" data-dismiss="modal">&times;</button>
+            <h4 class="modal-title">Found</h4>
+          </div>
+          <div class="modal-body">
+            <p>Enter Found Item.</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button2" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+    
+  </div>
+  <div id='map' style='width: 960px; height: 600px ; margin-left: .05%'></div>
         <pre id='info'></pre>
 <script>
 
@@ -78,62 +132,6 @@ map.on('mousemove', function (e) {
    });
 </script>
 
-        
-    
-   
-  <ol>
-    <li><a href="lost.php">lost.php</a></li>
-    <li><a href="found.php">found.php</a></li>
-    <li><a href="about.php">about.php</a></li>
-  </ol>
-
-  <div class="container">
-    <!-- Trigger the modal with a button -->
-    <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Lost</button>
-    <button type="button2" class="btn btn-info btn-lg" data-toggle="modal"
-    data-target="#myModal2">Found</button>
-
-    <!-- Modal -->
-    <div class="modal fade" id="myModal" role="dialog">
-      <div class="modal-dialog">
-      
-        <!-- Modal content-->
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal">&times;</button>
-            <h4 class="modal-title">Lost</h4>
-          </div>
-          <div class="modal-body">
-            <p>Enter Lost Item.</p>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-          </div>
-        </div>
-        
-      </div>
-    </div>
-    <div class="modal fade" id="myModal2" role="dialog">
-      <div class="modal-dialog">
-      
-        <!-- Modal content-->
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button2" class="close" data-dismiss="modal">&times;</button>
-            <h4 class="modal-title">Found</h4>
-          </div>
-          <div class="modal-body">
-            <p>Enter Found Item.</p>
-          </div>
-          <div class="modal-footer">
-            <button type="button2" class="btn btn-default" data-dismiss="modal">Close</button>
-          </div>
-        </div>
-        
-      </div>
-    </div>
-    
-  </div>
 
 </body>
 </html>
