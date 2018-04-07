04ef4f1bf53b48f62b455fd083fbe61a1463b406
Joshua-Newton <Joshua.Newton@uvm.edu>
2018-04-07 10:34:31 -0400

    Added a paragraph to the about page.

--- src/app/about.php
+++ src/app/about.php
@@ -1,3 +1,16 @@
 <?php
+include 'top.php'
+?>
 
+<h1>About UVM Lost and Found</h1>
+<p>UVM lost and found was created to help people find lost items with as little hassle as possible.
+   This site will allow people to find where their lost items may be with an easy to use map that 
+   shows the locations of people's belongings. This map is only open to UVM students, so there is
+   no fear of random people taking your things.</p>
+
+<p>Everyone who uses this site has to enter their net ID and password, meaning that we can trackage
+   who is reliable and who is not.</p>
+
+<?php
+include 'footer.php'
 ?>
