6a0e806d683bc23857026de9e3663714d5bc7f0f
rbaranov96 <33300327+rbaranov96@users.noreply.github.com>
2018-04-07 10:41:25 -0400

    Add files via upload

--- src/app/home.php
+++ src/app/home.php
@@ -23,7 +23,7 @@
     <!-- Trigger the modal with a button -->
     <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Lost</button>
     <button type="button2" class="btn btn-info btn-lg" data-toggle="modal"
-    data-target="#myModal">Found</button>
+    data-target="#myModal2">Found</button>
 
     <!-- Modal -->
     <div class="modal fade" id="myModal" role="dialog">
@@ -45,17 +45,17 @@
         
       </div>
     </div>
-    <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal fade" id="myModal2" role="dialog">
       <div class="modal-dialog">
       
         <!-- Modal content-->
         <div class="modal-content">
           <div class="modal-header">
             <button type="button2" class="close" data-dismiss="modal">&times;</button>
-            <h4 class="modal-title">Founf</h4>
+            <h4 class="modal-title">Found</h4>
           </div>
           <div class="modal-body">
-            <p>Enter Founf Item.</p>
+            <p>Enter Found Item.</p>
           </div>
           <div class="modal-footer">
             <button type="button2" class="btn btn-default" data-dismiss="modal">Close</button>
