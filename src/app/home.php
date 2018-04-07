d338dd62f074727d37020d09121af9ff9e88df2f
Joshua-Newton <Joshua.Newton@uvm.edu>
2018-04-07 09:18:35 -0400

    Adding the home page, basic stuff so far.

---
+++ src/app/home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<HTML lang="en">
+
+    <head>
+        <title>CS 8 ROCKS</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Codefest Team 7">
+        <meta name="description" content="A home page for the codefest project">
+        <!-- Style -->
+    </head>
+    
+    <body>
+        <p>This is the beginning of a great lost and found project.</p>
+    </body>
+</html>
