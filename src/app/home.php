a76af829f025ab1ee2df34af189b99508945e766
wsoccorsi <wsoccors@uvm.edu>
2018-04-07 12:34:34 -0400

    hiiiiiiii

--- src/app/home.php
+++ src/app/home.php
@@ -1,6 +1,8 @@
 
 <!DOCTYPE html>
-<?php  include "top.php"; ?>
+
+<?php include "top.php"; ?>
+
 <html lang="en">
 <head>
   <title>litterBox</title>
@@ -10,9 +12,11 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.44.1/mapbox-gl.js'></script>
+<link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.44.1/mapbox-gl.css' rel='stylesheet' />
 </head>
 <body>
-  <p>This is the beginning of a great lost and found project.</p
+ 
   <ol>
     <li><a href="lost.php">lost.php</a></li>
     <li><a href="found.php">found.php</a></li>
@@ -20,16 +24,16 @@
   </ol>
 
   <div class="container">
-    <!-- Trigger the modal with a button -->
+     
     <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Lost</button>
     <button type="button2" class="btn btn-info btn-lg" data-toggle="modal"
     data-target="#myModal2">Found</button>
 
-    <!-- Modal -->
+    
     <div class="modal fade" id="myModal" role="dialog">
       <div class="modal-dialog">
       
-        <!-- Modal content-->
+         
         <div class="modal-content">
           <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal">&times;</button>
@@ -48,7 +52,7 @@
     <div class="modal fade" id="myModal2" role="dialog">
       <div class="modal-dialog">
       
-        <!-- Modal content-->
+         
         <div class="modal-content">
           <div class="modal-header">
             <button type="button2" class="close" data-dismiss="modal">&times;</button>
@@ -66,7 +70,71 @@
     </div>
     
   </div>
+  
+  
+  <div id='map' style='width: 960px; height: 600px ; margin-top: 0%; margin-left: 10%;'></div>
+        <pre id='info'></pre>
+<script>
+mapboxgl.accessToken = 'pk.eyJ1Ijoiam9uYXRoYW4tbGV2ZW50aGFsIiwiYSI6ImNqZm9sZjdzYzA3Y3gzMnNibGpxM3BueDMifQ.1JFSsOoXxKPQoQbbfrVYAA';
+var map = new mapboxgl.Map({
+   container: 'map',
+   style: 'mapbox://styles/mapbox/light-v9',
+   center: [-73.201059,44.478076], // starting position
+   zoom: 12 // starting zoom
+   
+});
+map.addControl(new mapboxgl.NavigationControl());
+map.on('load', function () {
+
+   map.addLayer({
+       "id": "points",
+       "type": "symbol",
+       "source": {
+           "type": "geojson",
+           "data": {
+               "type": "FeatureCollection",
+               "features": [{
+                   "type": "Feature",
+                   "geometry": {
+                       "type": "Point",
+                       "coordinates": [-77.03238901390978, 38.913188059745586]
+                   },
+                   "properties": {
+                       "title": "Mapbox DC",
+                       "icon": "monument"
+                   }
+               }, {
+                   "type": "Feature",
+                   "geometry": {
+                       "type": "Point",
+                       "coordinates": [-122.414, 37.776]
+                   },
+                   "properties": {
+                       "title": "Mapbox SF",
+                       "icon": "harbor"
+                   }
+               }]
+           }
+       },
+       "layout": {
+           "icon-image": "{icon}-15",
+           "text-field": "{title}",
+           "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+           "text-offset": [0, 0.6],
+           "text-anchor": "top"
+       }
+   });
+});
+map.on('mousemove', function (e) {
+   document.getElementById('info').innerHTML =
+       // e.point is the x, y coordinates of the mousemove event relative
+       // to the top-left corner of the map
+       JSON.stringify(e.point) + '<br />' +
+       // e.lngLat is the longitude, latitude geographical position of the event
+       JSON.stringify(e.lngLat);
+   });
+</script>
+
 
 </body>
 </html>
-<?php include "footer.php";?>
