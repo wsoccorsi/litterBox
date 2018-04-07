0a115cd3b9ec73cb192e3ae51930571e3cfcf4b5
rbaranov96 <33300327+rbaranov96@users.noreply.github.com>
2018-04-07 10:36:39 -0400

    Add files via upload

--- src/app/home.php
+++ src/app/home.php
@@ -42,13 +42,20 @@
             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
           </div>
         </div>
+        
+      </div>
+    </div>
+    <div class="modal fade" id="myModal" role="dialog">
+      <div class="modal-dialog">
+      
+        <!-- Modal content-->
         <div class="modal-content">
           <div class="modal-header">
             <button type="button2" class="close" data-dismiss="modal">&times;</button>
-            <h4 class="modal-title">Lost</h4>
+            <h4 class="modal-title">Founf</h4>
           </div>
           <div class="modal-body">
-            <p>Enter Found Item.</p>
+            <p>Enter Founf Item.</p>
           </div>
           <div class="modal-footer">
             <button type="button2" class="btn btn-default" data-dismiss="modal">Close</button>
